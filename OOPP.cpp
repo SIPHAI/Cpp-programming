c93e5500257559390f2e915d039629a24f7b92e7
Siphai PHOL <100112002+SIPHAI@users.noreply.github.com>
2022-08-17 17:05:40 +0700

    Add files via upload

---
+++ OOPP.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int cout = 0;
+    cout << endl << "The is the student name and score .";
+    cin >> count;
+     cout<<endl
+         << 
+}
