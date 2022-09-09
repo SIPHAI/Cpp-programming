da14b058faf551e05fe0effb9a9efbc7bdf3b684
Siphai PHOL <100112002+SIPHAI@users.noreply.github.com>
2022-09-09 23:34:01 +0700

    Add files via upload

---
+++ Ex00.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    cout << "\n\n Hello C++ :\n";
+	cout << "----------------------------------------------\n";
+    cout << " Welcome to C++ and Programiz \n" ;
+    cout << " I'm SaiPy = Siphai haha "<<endl ;
+    
+}
